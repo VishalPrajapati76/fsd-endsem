e259a371b286f2125f7d3e12ee848aea6fea6cac
Vishal Prajapati <vishal@student.com>
2026-05-19 10:08:54 +0530

    chore: add root gitignore

---
+++ commit.bat
@@ -0,0 +1,4 @@
+cd /d C:\Users\VISHAL PRAJAPATI\Desktop\fsd-final
+git config user.email "vishal@student.com"
+git config user.name "Vishal Prajapati"
+git commit -m "feat: complete MERN AI Complaint Management System"
