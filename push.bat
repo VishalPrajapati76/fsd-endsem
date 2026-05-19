f5dd82db58061c85351d600b1be0727e18c80fed
Vishal Prajapati <vishal@student.com>
2026-05-19 10:27:13 +0530

    main

---
+++ push.bat
@@ -0,0 +1,4 @@
+cd /d C:\Users\VISHAL PRAJAPATI\Desktop\fsd-final
+git add frontend/.env backend/server.js
+git commit -m "fix: set production backend URL for Render deployment"
+git push origin main
